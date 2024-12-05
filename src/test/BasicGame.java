f10b315a375a34e2648b6658ced44b91ef3c1ec4
Zachary <513661@student.saxion.nl>
2024-12-05 14:23:01 +0100

    backlog item 10 - added Clickable object and room

--- src/test/BasicGame.java
+++ src/test/BasicGame.java
@@ -1,9 +1,10 @@
-package test;
 import nl.saxion.app.SaxionApp;
 
 import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
+
+import java.awt.*;
 
 public class BasicGame implements GameLoop {
 
@@ -11,14 +12,30 @@ public class BasicGame implements GameLoop {
         SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
     }
 
+    private static Room r1;
+    //private static final String img2 = "basicGame/2.png";
+    //private static final String img3 = "basicGame/3.png";
+
+//    coordinates and size for flower
+
+//    coordinates and size for light
+
+
+
+
+
+
+
     @Override
     public void init() {
+        r1= new Room("basicGame/1.png");
 
     }
 
     @Override
     public void loop() {
-
+        SaxionApp.clear();
+        r1.draw();
     }
 
     @Override
@@ -28,8 +45,21 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void mouseEvent(MouseEvent mouseEvent) {
-
+        int mouseX = mouseEvent.getX();
+        int mouseY = mouseEvent.getY();
+        String position = String.format("the position of the object: X: %d and Y: %d", mouseX,mouseY);
+        String output=r1.getClick(mouseX,mouseY);
+        System.out.println("Output of click: "+output);
+        System.out.println(position);
+        SaxionApp.setFill(Color.white);
+        SaxionApp.drawRectangle(0,0,1000,200);
+        SaxionApp.setTextDrawingColor(Color.black);
+        SaxionApp.drawText(position,10,10,20);
+        SaxionApp.setFill(Color.RED);
+        SaxionApp.drawRectangle(mouseX - 50 ,mouseY - 50 , 100,100);
     }
+
+
 }
 
 
