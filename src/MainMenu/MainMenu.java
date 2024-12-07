a6fc77ff77317345fb385f63e8a84e002341e7fd
MD JUBAIDUL HAQUE <alexnishat@gmail.com>
2024-12-07 11:28:45 +0100

    i did all the code but unfortunately its not running . could you check it .

--- src/MainMenu/MainMenu.java
+++ src/MainMenu/MainMenu.java
@@ -2,6 +2,7 @@ package MainMenu;
 
 import nl.saxion.app.SaxionApp;
 import nl.saxion.app.interaction.GameLoop;
+import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
 
 import java.awt.*;
@@ -14,7 +15,7 @@ public class BasicGame implements GameLoop {
         SaxionApp.startGameLoop(new BasicGame(), 800, 600, 30);
     }
 
-    @Override
+
     public void processInput(MouseEvent mouseEvent) {
         if (!startGame && mouseEvent.isLeftClicked()) {
             int mouseX = mouseEvent.getX();
@@ -25,12 +26,12 @@ public class BasicGame implements GameLoop {
         }
     }
 
-    @Override
+
     public void update() {
         // Game logic goes here
     }
 
-    @Override
+
     public void render() {
         SaxionApp.clear();
 
@@ -41,5 +42,25 @@ public class BasicGame implements GameLoop {
         } else {
             SaxionApp.drawText("Game is running...", 300, 300, 30, Color.BLACK);
         }
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void keyboardEvent(KeyboardEvent keyboardEvent) {
+
+    }
+
+    @Override
+    public void mouseEvent(MouseEvent mouseEvent) {
+
     }
 }
