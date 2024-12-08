c492823d6ef4c2fcd957d2763ae9ea20067a8f0d
Zachary <513661@student.saxion.nl>
2024-12-08 18:12:36 +0100

    backlog item - organized some of the backgroundTemplate code and commented on stuff for understandability V2

--- src/backgroundTemplate/Template.java
+++ src/backgroundTemplate/Template.java
@@ -23,25 +23,25 @@ public class Template implements GameLoop {
         rooms = new BackgroundScene[4];// Change the 4 to however much more rooms there are
 
         // Keep adding the amount of rooms needed and amount of props, in the order of the rooms pls <3
-        rooms[0]=new BackgroundScene("resources/1.png"); // Room image
+        rooms[0]=new BackgroundScene("resources/1.png"); // Room 1 image
         rooms[0].clickablePropLocation(100, 100, 50, 50, "prop 1 in room 1"); // Prop location
         rooms[0].clickablePropLocation(200, 200, 50, 50, "prop 2 in room 1");
 
-        rooms[1]=new BackgroundScene("resources/2.png"); // Room image
+        rooms[1]=new BackgroundScene("resources/2.png"); // Room 2 image
         rooms[1].clickablePropLocation(150, 150, 50, 50, "prop 1 in room 2"); // Prop location
         rooms[1].clickablePropLocation(250, 250, 50, 50, "prop 2 in room 2");
 
-        rooms[2]=new BackgroundScene("resources/3.png"); // Room image
+        rooms[2]=new BackgroundScene("resources/3.png"); // Room 3 image
         rooms[2].clickablePropLocation(300, 300, 50, 50, "prop 1 in room 3"); // Prop location
         rooms[2].clickablePropLocation(400, 400, 50, 50, "prop 2 in room 3");
 
-        rooms[3]=new BackgroundScene("resources/bat.jpeg"); // Room image
+        rooms[3]=new BackgroundScene("resources/bat.jpeg"); // Room 4 image
         rooms[3].clickablePropLocation(350, 350, 50, 50, "prop 1 in room 4"); // Prop location
         rooms[3].clickablePropLocation(450, 450, 50, 50, "prop 2 in room 4");
 
 
         currentRoom=0; // Initializing what room to start on
-        clickTime=0; // Initialize clickTime
+        clickTime=0; // Initializing clickTime
     }
 
     @Override
@@ -73,10 +73,6 @@ public class Template implements GameLoop {
             else if (output.equals("previousRoom")) { // Previous room
                 currentRoom = (currentRoom - 1 + rooms.length) % rooms.length;
             }
-
-            // Display clicked position with a red square
-            SaxionApp.setFill(Color.RED);
-            SaxionApp.drawRectangle(mouseX - 25, mouseY - 25, 50, 50);
 
             clickTime=currentTime; // Update clickTime
         }
