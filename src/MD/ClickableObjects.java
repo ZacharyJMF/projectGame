97a7451e752b72f0641ebf3623e9099a9d5b3c85
Zachary <513661@student.saxion.nl>
2024-12-12 15:27:45 +0100

    Added background template to MD package

---
+++ src/MD/ClickableObjects.java
@@ -0,0 +1,42 @@
+package MD;
+
+import nl.saxion.app.SaxionApp;
+import nl.saxion.app.canvas.drawable.Image;
+
+import java.awt.*;
+
+public class ClickableObjects {
+    public int x,y,width,height;
+    public String message;
+    private Image lImage;
+    private Image rImage;
+
+    public ClickableObjects(int x, int y, int width, int height, String message) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.message = message;
+    }
+
+    // Checks if coordinates are withing bounds of clickable object
+    public boolean isMe(int x, int y){
+        return x >= this.x && y >= this.y && x < this.x + width && y < this.y + height;
+    }
+
+    // Drawing clickable object location
+    // Make invisible and display image when clicked on
+    public void drawSwitches(){
+        SaxionApp.setFill(Color.yellow);
+        SaxionApp.drawRectangle(x,y,width,height);
+    }
+    public void drawProps(){
+        SaxionApp.setFill(Color.black);
+        SaxionApp.drawRectangle(x,y,width,height);
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
