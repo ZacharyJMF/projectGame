6ecd0436dbff9cab14e1b3993e1ee5a52622b6c3
armineslamieh <117245711+armineslamieh@users.noreply.github.com>
2024-12-18 14:20:24 +0100

    I made a class out of the safeBoxApp file for using within the main template file. Also I have added the class to the findingCraig package and added the esential codes to the template in order to run the safebox class.

--- src/FindingCraig/Template.java
+++ src/FindingCraig/Template.java
@@ -13,45 +13,29 @@ public class Template implements GameLoop {
 
     private Rooms rooms;
     private SceneClicking sceneClicking;
+    private safeBox safeBox;
+
 
     public static void main(String[] args) {
+
         SaxionApp.startGameLoop(new Template(), 1000, 1000, 40);
     }
 
-    String safeboxImg = "resources/safe-box.png";
-    ArrayList<String> enteredNumbers = new ArrayList<>();
-    private long clickTime;
-    int passcode = 250;
-    int[][] numpadBounds = {
-            {545, 429, 50, 50, 1}, // 1
-            {614, 429, 50, 50, 2}, // 2
-            {682, 429, 50, 50, 3}, // 3
-            {545, 511, 50, 50, 4}, // 4
-            {614, 511, 50, 50, 5}, // 5
-            {682, 511, 50, 50, 6}, // 6
-            {545, 590, 50, 50, 7}, // 7
-            {614, 590, 50, 50, 8}, // 8
-            {682, 590, 50, 50, 9}, // 9
-            {613, 669, 50, 50, 0}  // 0
-    };
 
     @Override
     public void init() {
-
         rooms = new Rooms();
         sceneClicking = new SceneClicking(rooms, 0);
-
         long clickTime = 0;
-        SaxionApp.drawImage(safeboxImg, 0, 0, 1000, 1000);
-        SaxionApp.drawText("Entered Numbers:", 400, 100, 30);
+        safeBox = new safeBox();
+        safeBox.redraw();
     }
 
     @Override
     public void loop() {
-        SaxionApp.clear();
-        rooms.getRoom(sceneClicking.getCurrentRoom()).draw();
-    }
+//        rooms.getRoom(sceneClicking.getCurrentRoom()).draw();
 
+    }
 
     @Override
     public void keyboardEvent(KeyboardEvent keyboardEvent) {
@@ -62,49 +46,8 @@ public class Template implements GameLoop {
     public void mouseEvent(MouseEvent mouseEvent) {
         // Call mouseEvent in SceneClicking
         sceneClicking.handleMouseEvent(mouseEvent);
-
-        long currentTime = System.currentTimeMillis();
-        if (currentTime - clickTime > 300) {
-            if (mouseEvent.isLeftMouseButton()) {
-                double mouseX = mouseEvent.getX();
-                double mouseY = mouseEvent.getY();
-
-
-                for (int[] bound : numpadBounds) {
-                    int x = bound[0];
-                    int y = bound[1];
-                    int width = bound[2];
-                    int height = bound[3];
-                    String number = String.valueOf(bound[4]);
-
-                    if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
-                        enteredNumbers.add(number);
-                    }
-                    String displayText = String.join("", enteredNumbers);
-                    SaxionApp.drawText(displayText, 400, 150, 40);
-
-                    while (enteredNumbers.size() == 3){
-                        if(Integer.valueOf(displayText) == passcode){
-                            SaxionApp.clear();
-                            SaxionApp.drawText("you passed this puzzle", 300, 500, 40);
-                            break;
-                        }else {
-                            enteredNumbers.clear();
-                            redraw();
-                            break;
-                        }
-
-                    }
-                }
-            }
-            clickTime = currentTime;
-        }
-    }
-    
-    private void redraw() {
-        SaxionApp.clear();
-        SaxionApp.drawImage(safeboxImg, 0, 0, 1000, 1000);
-        SaxionApp.drawText("Entered Numbers:", 400, 100, 30);
+        safeBox.handleMouseEvent(mouseEvent);
 
     }
+
 }
